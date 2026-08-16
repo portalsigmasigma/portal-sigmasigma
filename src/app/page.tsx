b474ecf47bea865f606ea024aa696dc48f26b71d
Danilo Chierici <danilo.chierici@gmail.com>
2026-08-16 11:39:10 -0300

    Fase 4-botao melhore scroll melhor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+// Página inicial simples que renderiza a seção Hero
 import Hero from '@/components/Hero';
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-fundo flex flex-col justify-center">
       <main className="w-full">
+        {/* Componente Hero com os botões apontando para /disciplinas e /materiais */}
         <Hero />
       </main>
     </div>
